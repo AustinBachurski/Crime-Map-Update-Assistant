f649533b2c76704c4cae977326b227fc8171f1b1
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-05-05 05:30:03 -0600

    read path from file

--- src/UploadFrame.hpp
+++ src/UploadFrame.hpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 #include <filesystem>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <wx/clipbrd.h>
 #include <wx/wx.h>
@@ -15,10 +17,16 @@ public:
 	~UploadFrame();
 
 private:
-	const std::string m_appendFeatures;
-	const std::string m_deleteFeatures;
-	const std::string m_tableToPoint;
+	// Python scripts.
+	const std::string m_appendFeaturesScript;
+	const std::string m_deleteFeaturesScript;
+	const std::string m_tableToPointScript;
 
+	// Path to subprocess folder.
+	const std::string m_pathFile;
+
+	// Methods
+	std::string callPython(const std::string& script);
 	void exit(wxCommandEvent& evt);
 	void help(wxCommandEvent& evt);
 	void next(wxCommandEvent& evt);
